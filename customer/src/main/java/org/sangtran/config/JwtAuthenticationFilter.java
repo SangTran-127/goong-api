702d03012458298abb0dfeac28314d1ff333125b
SangTran-127 <tranquangsang12.7@gmail.com>
2023-03-24 17:26:05 +0700

    add Application config

--- customer/src/main/java/org/sangtran/config/JwtAuthenticationFilter.java
+++ customer/src/main/java/org/sangtran/config/JwtAuthenticationFilter.java
@@ -7,6 +7,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.sangtran.service.JwtService;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,6 +19,7 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
     @Override
     protected void doFilterInternal(
            @NonNull HttpServletRequest request,
@@ -30,6 +34,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
         jwt = authHeader.substring(7);
         String userEmail = jwtService.extractUsername(jwt);
-
+        // user not authen
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // get user from the db
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+        }
     }
 }
